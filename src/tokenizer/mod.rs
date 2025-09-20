7234136151a03c35cc4f0d71d4067a9286d63496
Slacker201 <122690152+Slacker201@users.noreply.github.com>
2025-09-20 10:12:39 -0600

    created keyword_symbol_parser module

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -5,12 +5,12 @@ use crate::{lexer_error::LexerError, lexer_token::LexerToken, tokenizer::comment
 
 
 mod comment_literal_parse;
-
+mod keyword_symbol_parser;
 
 pub(crate) fn tokenize(program: String) -> Result<Vec<LexerToken>, LexerError> {
     let first_pass_tokens = strip_literals_and_comments(program);
     println!("{:?}", first_pass_tokens);
-
+    let second_pass_tokens = parse_keywords_symbols(first_pass_tokens);
 
     Ok(Vec::new())
 }
