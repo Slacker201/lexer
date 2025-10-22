6362bf0d4c1f19d097f7d93e9eab44c5000ad35c
Slacker201 <slacker1234567809@gmail.com>
2025-10-22 16:14:08 -0600

    reset

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,5 @@
-use crate::{lexer_error::LexerError, lexer_token::LexerToken, tokenizer::tokenize};
-
-pub mod lexer_token;
-pub mod lexer_error;
-mod tokenizer;
-
-pub fn lex(program: String, file: String) -> Result<Vec<LexerToken>, LexerError> {
-    tokenize(program, file)
-}
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn main() {
-        let program = "\"test\"".to_string();
-        let tokens = lex(program, "test_file".to_string()).unwrap();
-        println!("tokens: {:?}", tokens);
-    }
-}
+
+
